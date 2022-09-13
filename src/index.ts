e5831ce891677a654d057f486108ec5817e753b4
Amirhanov Said <saidderbentskij@yandex.ru>
2022-09-13 20:54:15 +0300

    fix commit

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,17 @@
 import express, {Request , Response} from  'express'
+//import bodyParser from 'body-parser'  //
+//import cors from 'cors'  //
+
 const app = express()
-const port = 5000
+
+//const corsMiidleware = cors(); //
+//app.use(corsMiidleware)  //
+//const jsonBodyMiddleware = bodyParser.json()  //
+//app.use(jsonBodyMiddleware) //
+
+const port = process.env.PORT || 5000
+
+//let videos = [...] //
 
 app.get('/', (req: Request , res:Response) => {
     let helloMessage = 'Hello INCUBATOR!!!';
