5db95a18fa4278d41d9feb8767f5ee65fb760e31
Amirhanov Said <saidderbentskij@yandex.ru>
2022-09-14 14:06:30 +0300

    new commit

--- src/index.ts
+++ src/index.ts
@@ -1,24 +1,91 @@
 import express, {Request , Response} from  'express'
-//import bodyParser from 'body-parser'  //
-//import cors from 'cors'  //
+import bodyParser from 'body-parser'
 
+//create express app
 const app = express()
-
-//const corsMiidleware = cors(); //
-//app.use(corsMiidleware)  //
-//const jsonBodyMiddleware = bodyParser.json()  //
-//app.use(jsonBodyMiddleware) //
 
 const port = process.env.PORT || 5000
 
-//let videos = [...] //
+const products = [{id:1, title: 'tomato'},{id: 2, title:'orange'}]
+const addresses = [{id: 1, value: 'Icrhi 12'},{id: 2, value: 'Lenina 11'}]
 
-app.get('/', (req: Request , res:Response) => {
-    let helloMessage = 'Hello INCUBATOR!!!';
-    res.send(helloMessage)
+const parserMiddleware = bodyParser({})
+app.use(parserMiddleware)
 
+app.get('/products',(req: Request,res: Response) => {
+    if (req.query.title){
+        let searchString = req.query.title.toString();
+        res.send(products.filter(p => p.title.indexOf(searchString) > -1))
+    } else{
+        res.send(products)
+    }
+    res.send(products)
+})
+app.post('/products',(req: Request,res: Response) => {
+    const newProduct = {
+        id: +(new Date()),
+        title: req.body.title
+    }
+    products.push(newProduct)
+    res.status(201).send(newProduct)
+})
+// app.get('/products/:productTitle',(req: Request,res: Response) => {
+//     let product = products.find(p => p.title === req.params.productTitle )
+//     if (product) {
+//         res.send(product)
+//     } else {
+//         res.send(404)
+//     }
+// })
+app.get('/products/:id',(req: Request,res: Response) => {
+    let product = products.find(p => p.id === +req.params.id )
+    if (product) {
+        res.send(product)
+    } else {
+        res.send(404)
+    }
+})
+app.put('/products/:id',(req: Request,res: Response) => {
+    let product = products.find(p => p.id === +req.params.id )
+    if (product) {
+        product.title = req.body.title
+        res.send(product)
+    } else {
+        res.send(200)
+    }
+})
+app.delete('/products/:id',(req: Request,res: Response) => {
+    for(let i=0; i < products.length; i++) {
+        if (products[i].id === +req.params.id) {
+            products.splice(i, 1);
+            res.send(204)
+            return;
+        }
+    }
+
+    res.send(404)
+})
+app.get('/addresses',(req: Request,res: Response) => {
+    res.send(addresses)
+})
+app.get('/addresses/:id',(req: Request,res: Response) => {
+    const id = +req.params.id;
+    let address = addresses.find(p => p.id === id)
+    if (address) {
+        res.send(address)
+    } else {
+        res.send(404)
+    }
 })
 
+//starp app
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
 })
+
+
+// app.get('/', (req: Request , res:Response) => {
+//     let helloMessage = 'Hello INCUBATOR!!!';
+//     res.send(helloMessage)
+//
+// })
